55e29058c57210b400e6fcdbe4a54aae22487a78
Deep Kidiya <kidiyadeep@gmail.com>
2022-12-30 10:59:31 +0530

    done theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import { React, createContext, useState } from "react";
+import { React } from "react";
 import "./App.css";
 
 import { Navbar } from "./components";
 
-export const ThemeContext = createContext(null);
 
 const App = () => {
-  const [theme, setTheme] = useState("dark");
-
-  const toggleTheme = () => {
-    setTheme((curr) => (curr === "dark" ? "light" : "dark"));
-  };
 
   return (
-    <ThemeContext.Provider value={{theme, toggleTheme}}>
-      <div className="app" id={theme}>
+      <div className="app">
         <div className="gradient__bg">
           <Navbar />
         </div>
       </div>
-    </ThemeContext.Provider>
   );
 };
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Logo_dark from "../../assets/logo.svg";
 import Logo_ligth from "../../assets/logo-light.svg";
 import { AiOutlineUser } from "react-icons/ai";
+import { RxSun } from "react-icons/rx";
+import { IoMoonOutline } from "react-icons/io5";
 
 import "./navbar.css";
 
 const Navbar = () => {
+  const [theme, setTheme] = useState('dark')
+  const [icon, setIcon] = useState('dark')
+  const toggleTheme = () => {
+    if(theme === "light") {
+      setTheme("dark")
+    } else {
+      setTheme("light")
+    }
+    setIcon(!icon)
+  };
+
+  useEffect(() => {
+    document.body.id = theme
+  }, [theme]);
+
   return (
     <div className="navbar">
       <div className="nav-container">
         <div className="nav-logo">
-          <img src={Logo_dark} alt="Logo" className="dark-logo" />
-          <img src={Logo_ligth} alt="Logo" className="ligth-logo" />
+          <img src={icon ? (Logo_dark) : (Logo_ligth)} alt="Logo" />
         </div>
         <div className="nav-links">
           <h2 className="nav-link">Marketplace</h2>
           <h2 className="nav-link">Rankings</h2>
           <h2 className="nav-link">Connect a wallet</h2>
-          <button className="nav-btn"><AiOutlineUser /> <span>Sign Up</span></button>
+          <button className="nav-btn">
+            <AiOutlineUser /> <span>Sign Up</span>
+          </button>
+          <button className="theme-btn" onClick={() => toggleTheme()}>
+          {icon ? <RxSun /> : <IoMoonOutline />}
+          </button>
         </div>
       </div>
     </div>
